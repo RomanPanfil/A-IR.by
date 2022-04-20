54085bf6311843cbe66f1e5a0507e788cfe0bf3d
arichild <arieevq@gmail.com>
2022-04-20 17:23:47 +0300

    save

--- dist/js/arina.js
+++ dist/js/arina.js
@@ -83,23 +83,40 @@ function setRating(rating) {
   }
 }
 
-btnAccordion.addEventListener('click', function() {
-  console.log(contentAccordion)
-  contentAccordion.classList.toggle('active');
-})
+// btnAccordion.addEventListener('click', function() {
+//   console.log(contentAccordion)
+//   contentAccordion.classList.toggle('active');
+// })
 
-if (matchMedia) {
-  var screen1170 = window.matchMedia("(max-width:1170px)");
-  screen1170.addListener(changes);
-  changes(screen1170);
+var acc = document.getElementsByClassName("accordion-items-title");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
 }
 
-console.log($('.tabs-item .content'))
+if (matchMedia) {
+  var screen1024 = window.matchMedia("(max-width:1024px)");
+  screen1024.addListener(changes);
+  changes(screen1024);
+}
 
 function changes(screen768) {
   if (screen768.matches) {
-    $('.accordion-content').appendTo($(".tabs-items"));
+    $('.accordion-content').appendTo($(".accordion"));
   } else {
-    $('.accordion-content').appendTo($(".tabs-item"));
+    $('.accordion-content').appendTo($(".tabs-items #tab-1"));
   }
 }
