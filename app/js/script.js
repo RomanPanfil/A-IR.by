0c527d22b83c2ca80a1223125f65933b35f9568e
DmitryPashkou <86959216+DmitryPash@users.noreply.github.com>
2022-04-20 12:37:14 +0100

    footer save
    footer save

--- app/js/script.js
+++ app/js/script.js
@@ -7,3 +7,40 @@ arrow.addEventListener("click", function () {
     numbers.classList.add("numbers-open");
   }
 });
+
+// Footer change media
+if (matchMedia) {
+  var screen1170 = window.matchMedia("(max-width:1170px)");
+  screen1170.addListener(changes);
+  changes(screen1170);
+}
+
+function changes(screen1170) {
+  if (screen1170.matches) {
+    $(".footer-bottom-links").appendTo($(".footer-top-contact"));
+    $(".footer-top-wrapper").appendTo($(".footer-top-info"));
+    $(".footer-top-services").appendTo($(".footer-top-about"));
+    $(".footer-top-resource").appendTo($(".footer-bootom-media-links"));
+    $(".footer-bottom-info").appendTo($(".footer-bootom-media-links"));
+    $(".footer-bottom-reit").appendTo($(".footer-bottom-socials"));
+    // $('.col-f-phones').prependTo($('.footer__top > .row'))
+  } else {
+    $(".footer-bottom-links").appendTo($(".footer-bootom-media-links"));
+    $(".footer-top-services").appendTo($(".footer-top-services-wrapper"));
+    $(".footer-top-wrapper").appendTo($(".footer-top-about"));
+    $(".footer-top-resource").appendTo($(".footer-top-resource-container"));
+    $(".footer-bottom-info").appendTo($(".footer-bottom-info-wrapper"));
+    $(".footer-bottom-reit").appendTo($(".footer-bottom-reit-wrapper"));
+    // $(".footer__top .col-f-phones").insertAfter(
+    //   $(".footer__bottom .col-f-wide")
+    // );
+  }
+}
+
+// Footer toggler
+let menu = document.querySelectorAll(".footer-top-title");
+menu.forEach((element) => {
+  element.addEventListener("click", () => {
+    element.classList.toggle("footer-open");
+  });
+});
