7137994beb55f4bc736eecf143fca1bb3c6fd4fa
NazarJS <nazar.poluyanow@gmail.com>
2022-04-21 16:47:46 +0300

    push

--- app/js/slider.js
+++ app/js/slider.js
@@ -1,31 +1,42 @@
-// const swiper = new Swiper(".product-swiper", {
-//   // Optional parameters
-//   loop: true,
-//   slidesPerView: 4,
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-// });
-// let mql = window.matchMedia("(min-width: 992px)");
+const swiperNazz = new Swiper(".product-swiper", {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 4,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
-// window.addEventListener("resize", () => {
-//   if (mql.matches) {
-//     const swiper = new Swiper(".product-swiper", {
-//       // Optional parameters
-//       loop: true,
-//       slidesPerView: 4,
-//       // Navigation arrows
-//       navigation: {
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev",
-//       },
-//     });
-//   } else {
-//     swiper.destroy();
-//   }
-// });
+let mql = window.matchMedia("(min-width: 1260px)");
+
+window.addEventListener("resize", () => {
+  if (mql.matches) {
+    // const swiper = new Swiper(".product-swiper", {
+    //   // Optional parameters
+    //   loop: true,
+    //   slidesPerView: 4,
+    //   // Navigation arrows
+    //   navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    //   },
+    // });
+  } else {
+    console.log("done");
+    // swiperNazz.destroy();
+    document.querySelector(".zxc").classList.remove("swiper");
+    document.querySelector(".zxc-wrap").classList.remove("swiper-wrapper");
+    let arr = document.querySelectorAll(".zxc-slide");
+    arr.forEach((el) => {
+      el.classList.remove("swiper-slide");
+      el.classList.remove("swiper-slide-duplicate");
+      el.classList.remove("swiper-slide-duplicate-active");
+      el.classList.remove("swiper-slide-duplicate-next");
+    });
+  }
+});
 const slider1 = new Swiper(".el", {
   slidesPerView: 4,
   spaceBetween: 0,
