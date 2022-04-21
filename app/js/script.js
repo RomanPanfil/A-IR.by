7669b62590c60429cdace865670116d6f5b4bb8f
DmitryPashkou <86959216+DmitryPash@users.noreply.github.com>
2022-04-21 22:58:19 +0100

    save
    save

--- app/js/script.js
+++ app/js/script.js
@@ -44,10 +44,8 @@ function changes768(screen768) {
   if (screen768.matches) {
     $(".footer-bottom-socials-wrapper").appendTo($(".footer-top-about"));
     $(".footer-bottom-info").appendTo($(".footer-top-media-wrapper"));
- 
   } else {
-     $(".footer-bottom-socials-wrapper").appendTo($(".footer-bottom-socials"));
-
+    $(".footer-bottom-socials-wrapper").appendTo($(".footer-bottom-socials"));
   }
 }
 
@@ -56,5 +54,73 @@ let menu = document.querySelectorAll(".footer-top-title");
 menu.forEach((element) => {
   element.addEventListener("click", () => {
     element.classList.toggle("footer-open");
+  });
+});
+
+// Imask for calculator-input
+let calculatorMask = document.querySelectorAll(".calculator-mask");
+calculatorMask.forEach((element) => {
+  IMask(element, {
+    mask: /^[1-9]\d{0,9}$/,
+  });
+});
+// for input catalog-products
+let inputMask = document.querySelectorAll(".input-mask");
+inputMask.forEach((element) => {
+  IMask(element, {
+    mask: /^[0-9]\d{0,6}$/,
+  });
+});
+// Sticker hidden catalog.html
+
+let stickerHideBtn = document.querySelector(".sticker-hide");
+let stickerHiden = document.querySelector(".sticker-hidden");
+
+stickerHideBtn.onclick = () => {
+  stickerHiden.classList.toggle("sticker-hidden-open");
+  stickerHideBtn.innerHTML = "Скрыть";
+};
+
+// Input range
+
+const rangeInput = document.querySelectorAll(
+    ".catalog-products-range-input input"
+  ),
+  priceInput = document.querySelectorAll(".catalog-products-price-input input"),
+  range = document.querySelector(".catalog-products-slider .progress");
+let priceGap = 100000;
+priceInput.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    let minPrice = parseInt(priceInput[0].value),
+      maxPrice = parseInt(priceInput[1].value);
+
+    if (maxPrice - minPrice >= priceGap && maxPrice <= rangeInput[1].max) {
+      if (e.target.className === "input-min") {
+        rangeInput[0].value = minPrice;
+        range.style.left = (minPrice / rangeInput[0].max) * 100 + "%";
+      } else {
+        rangeInput[1].value = maxPrice;
+        range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
+      }
+    }
+  });
+});
+
+rangeInput.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    let minVal = parseInt(rangeInput[0].value),
+      maxVal = parseInt(rangeInput[1].value);
+    if (maxVal - minVal < priceGap) {
+      if (e.target.className === "range-min") {
+        rangeInput[0].value = maxVal - priceGap;
+      } else {
+        rangeInput[1].value = minVal + priceGap;
+      }
+    } else {
+      priceInput[0].value = minVal;
+      priceInput[1].value = maxVal;
+      range.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+      range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+    }
   });
 });
