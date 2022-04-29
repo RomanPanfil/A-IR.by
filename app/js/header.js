d494898ed95f2df29fc9371b075e21b614e5c706
Dmitry <86959216+DmitryPash@users.noreply.github.com>
2022-04-29 18:12:22 +0100

    save
    save

--- app/js/header.js
+++ app/js/header.js
@@ -1,20 +1,29 @@
-$(function () {
-  let header = $(".header"),
-    headerMenu = $(".header .menu"),
-    headerNav = $(".header .nav-wrapper"),
-    wrapperFixed = $(".wrapper");
+// if (matchMedia) {
+//   var screen1024 = window.matchMedia("(max-width:1024px)");
+//   screen1024.addListener(changes);
+//   changes(screen1024);
+// }
 
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 1) {
-      header.addClass("header_fixed");
-      wrapperFixed.addClass("wrapper_fixed");
-      headerMenu.hide();
-      headerNav.hide();
-    } else {
-      header.removeClass("header_fixed");
-      wrapperFixed.removeClass("wrapper_fixed");
-      headerMenu.show();
-      headerNav.show();
-    }
+
+  $(function () {
+    let header = $(".header"),
+      headerMenu = $(".header .menu"),
+      headerNav = $(".header .nav-wrapper"),
+      wrapperFixed = $(".wrapper");
+  
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 1) {
+        header.addClass("header_fixed");
+        wrapperFixed.addClass("wrapper_fixed");
+        headerMenu.hide();
+        headerNav.hide();
+      } else {
+        header.removeClass("header_fixed");
+        wrapperFixed.removeClass("wrapper_fixed");
+        headerMenu.show();
+        headerNav.show();
+      }
+    });
   });
-});
+
+
