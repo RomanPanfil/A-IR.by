6344ef08a3bd5f87912dae4a477128c7d5ef1e57
NazarJS <nazar.poluyanow@gmail.com>
2022-08-04 16:05:57 +0300

    end

--- dist/js/main.js
+++ dist/js/main.js
@@ -1616,3 +1616,54 @@ $('.main-list-li').click(function(){
 $('.waitList-item-remove-link').click(function(){
   $(this).closest($('div.waitList-item')).remove();
 });
+
+$(document).ready(function () {
+  $(".request-body-wrap-form").validate({
+    errorElement: "span",
+    rules: {
+      name: {
+        required: true,
+        // lettersonly: true,
+      },
+
+      textbox: {
+        required: true,
+        maxlength: 500,
+      },
+
+      url: {
+        url: true,
+        required: true,
+        // email: true,
+      },
+
+    },
+
+    messages: {
+      name: {
+        required: "Пожалуйста, введите ваше Наименование",
+        name: " ",
+      },
+
+      textbox: {
+        required: "Пожалуйста, оставьте свой комментарий",
+        maxlength: "Комментарий не должен превышать 500 символов",
+      },
+
+      url: {
+        required: "Введите данные",
+      }
+    },
+    highlight: function(element, errorClass, validClass) {
+      $(element).addClass(errorClass).removeClass(validClass);
+      $(element).closest('.ui-field').find('.popup-icon')
+        .addClass(errorClass).removeClass(validClass);
+      
+    },
+    unhighlight: function(element, errorClass, validClass) {
+      $(element).removeClass(errorClass).addClass(validClass);
+      $(element).closest('.ui-field').find('.popup-icon')
+        .removeClass(errorClass).addClass(validClass);
+    }
+  });
+})
